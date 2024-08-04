1d5fea8fbec3a6a2a3dd419a3fe9b1fc08ea4543
Brian Lam <anhlam051595@gmail.com>
2024-08-04 17:53:04 -0400

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Brian",
+                Dob = new DateTime(1995, 05,15)
+                
+            };
+            return View(data);
+        }
+    }
+}
